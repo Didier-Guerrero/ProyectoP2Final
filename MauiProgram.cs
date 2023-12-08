5a3503bd20f72fbb478d079edfeb98b45e90a5c6
Didier Guerrero <didier.guerrero@udla.ede.ec>
2023-12-08 16:23:02 -0500

    Creacion de pagina principal, adicion de font y flyout

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("Aclonica-Regular", "Aclonica-Regular");
+            });
 		var dbContext = new ReservaDbContext();
 		dbContext.Database.EnsureCreated();
 		dbContext.Dispose();
