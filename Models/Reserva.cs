a631826525f2677bfc6d603b39d57f675ffd4749
Didier Guerrero <didier.guerrero@udla.ede.ec>
2023-12-08 10:39:11 -0500

    Creacion Carpeta de modelos y modelo Reserva

---
+++ Models/Reserva.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProyectoP2Final.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int IdReserva { get; set; }
+        public String Nombre { get; set; }
+        public String Apeliido { get; set; }
+        [MaxLength(10)]
+        public String Cedula { get; set; }
+        public int NumeroHuespedes { get; set; }
+        public DateTime FechaEntrada { get; set; }
+        public DateTime FechaSalida { get; set; }
+    }
+}
